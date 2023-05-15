439ea7b565d15c001f420d7fe5dfab8c80caa596
canedoly <125576784+technikfratnik23@users.noreply.github.com>
2023-05-15 14:10:18 +0200

    dumb

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
-// Use this only when you want to host this website locally
 import http.server
 
 server = http.server.HTTPServer(('localhost', 8000), http.server.CGIHTTPRequestHandler)
 
 server.serve_forever()
+
+pause
